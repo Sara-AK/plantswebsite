8dd9ad321a2c0312654e837fd60b342f6e1a3117
Malak1213 <10121681@mu.edu.lb>
2025-01-15 22:12:12 +0200

    malakz first commit

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\GardenerController;
+use App\Http\Controllers\SellerController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\PostsController;
+
+
 Route::get('/plants', [PlantController::class, 'index'])->name('public.plants.index'); // List all plants
 Route::get('/plants/{id}', [PlantController::class, 'show'])->name('public.plants.show');
 
@@ -56,6 +64,49 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('plant-products', PlantProductController::class);
+
+// from me
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// user route
+Route::middleware('role:user')->group(function () {
+    Route::get('/user/dashboard', [UserController::class, 'index']);
+});
+
+// admin routes
+Route::middleware('role:admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index']);
+    Route::get('/admin/user/{id}', [AdminController::class, 'destroy']);
+});
+
+// gardener routes
+Route::middleware('role:gardener')->group(function () {
+    Route::get('/gardener/dashboard', [GardenerController::class, 'index']);
+    Route::get('/gardener/inquiries', [GardenerController::class, 'viewInquiries']);
+});
+
+// seller routes
+Route::middleware('role:seller')->group(function () {
+    Route::get('/seller/dashboard', [SellerController::class, 'index']);
+    Route::get('/seller/items', [SellerController::class, 'store']);
+});
+
+// Route::get('/register', [RegisteredUserController::class, 'create']);
+// Route::get('/register/{$id}', [RegisteredUserController::class, 'create']);
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
+
+Route::get('/posts', [PostsController::class, 'index'])->name('posts_index');
+
 
 
 require __DIR__ . '/auth.php';
